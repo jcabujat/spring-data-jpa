ec4b5fdb28c6788eb8c2e3d31e8735276b5b01f2
jonathancabujat <jonathancabujat@gmail.com>
2020-12-27 13:57:59 +0800

    First commit

---
+++ src/main/java/com/jcabujat/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -0,0 +1,87 @@
+package com.jcabujat.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.jcabujat.springboot.cruddemo.entity.Employee;
+
+@Repository
+public class EmployeeDAOHibernateImpl implements EmployeeDAO {
+
+	// define field for entitymanager
+	
+	private EntityManager entityManager;
+	
+	// setup constructor injection
+	@Autowired
+	public EmployeeDAOHibernateImpl(EntityManager entityManager) {
+		super();
+		this.entityManager = entityManager;
+	}
+	
+	@Override
+	public List<Employee> findAll() {
+		
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		// create a query
+		Query<Employee> theQuery =
+				currentSession.createQuery("from Employee", Employee.class);
+		
+		// execute query and result list
+		List<Employee> employees = theQuery.getResultList();
+		
+		// return the results
+		return employees;
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		
+		// get the current hibernate session
+		Session currentSession =
+				entityManager.unwrap(Session.class);
+		
+		// get the employee
+		Employee theEmployee = 
+				currentSession.get(Employee.class, theId);
+		
+		// return the employee
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		// save employee
+		currentSession.saveOrUpdate(theEmployee);
+		
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		// delete object with primary
+		Query theQuery =
+				currentSession.createQuery(
+						"delete from Employee where id=:employeeId");
+		theQuery.setParameter("employeeId", theId);
+		
+		theQuery.executeUpdate();
+		
+	}
+
+	
+}
---
+++ src/main/java/com/jcabujat/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package com.jcabujat.springboot.cruddemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jcabujat.springboot.cruddemo.dao.EmployeeDAO;
+import com.jcabujat.springboot.cruddemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	private EmployeeDAO employeeDAO;
+	
+	@Autowired	
+	public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+		this.employeeDAO = employeeDAO;
+	}
+
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+		return employeeDAO.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Employee findById(int theId) {
+		return employeeDAO.findById(theId);
+	}
+
+	@Override
+	@Transactional
+	public void save(Employee theEmployee) {
+		employeeDAO.save(theEmployee);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int theId) {
+		employeeDAO.deleteById(theId);
+	}
+
+}
